1035076778c307f475b5e30736451c75a2d466ed
MathewKasbarian <71024080+MathewKasbarian@users.noreply.github.com>
2021-03-19 02:39:30 -0400

    Delete assignment-2-master directory

--- assignment-2-master/src/main/java/ExpressionTree/TreeNode.java
+++
@@ -1,6 +0,0 @@
-package ExpressionTree;
-public interface TreeNode {
-//calls the classes in expression tree and prints it and evaluates it based on the depth
-   public abstract void print(int depth);
-   public abstract double evaluate();
-}
